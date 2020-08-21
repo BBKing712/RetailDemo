a771ae470018793a193cda86ef69c892bd5abde6
Bernhard Korn <b.korn@boservice.de>
2020-08-21 15:38:06 +0200

    DummyStuff added

---
+++ Messages/DummyStuffDone.cs
@@ -0,0 +1,15 @@
+﻿using NServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messages
+{
+    public class DummyStuffDone : IEvent
+    {
+        public string DummyStuffId { get; set; }
+
+    }
+}
---
+++ Shipping/DummyStuffDoneHandler.cs
@@ -0,0 +1,21 @@
+﻿using Messages;
+using NServiceBus;
+using NServiceBus.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shipping
+{
+    public class DummyStuffDoneHandler : IHandleMessages<DummyStuffDone>
+    {
+        static ILog log = LogManager.GetLogger<OrderPlacedHandler>();
+
+        public Task Handle(DummyStuffDone message, IMessageHandlerContext context)
+        {
+            log.Info($"Shipping has received DummyStuffDone, DummyStuffId = {message.DummyStuffId}");
+            return Task.CompletedTask;
+        }
+    }
+}
