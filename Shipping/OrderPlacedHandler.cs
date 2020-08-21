7cc59471cbdca0bcac962e974e0939154e4da135
Bernhard Korn <b.korn@boservice.de>
2020-08-21 15:27:39 +0200

    OrderPlacedHandler added

---
+++ Shipping/OrderPlacedHandler.cs
@@ -0,0 +1,20 @@
+﻿using Messages;
+using NServiceBus;
+using NServiceBus.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shipping
+{
+    public class OrderPlacedHandler : IHandleMessages<OrderPlaced>
+    {
+        static ILog log = LogManager.GetLogger<OrderPlacedHandler>();
+        public Task Handle(OrderPlaced message, IMessageHandlerContext context)
+        {
+            log.Info($"Shipping has received OrderPlaced, OrderId = {message.OrderId}");
+            return Task.CompletedTask;
+        }
+    }
+}
